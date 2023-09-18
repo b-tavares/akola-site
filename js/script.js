1f5c8ca13baa67ca15d701384fe71de5557b280c
Barbara Tavares <brbtavares@gmail.com>
2023-09-17 21:43:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    let scroll = document.querySelector('.scrollTop')
+        scroll.classList.toggle('active', window.scrollY > 450)
+})
+
+function backTop() {
+    window.scrollTo({
+        top: 0,
+    })
+}
